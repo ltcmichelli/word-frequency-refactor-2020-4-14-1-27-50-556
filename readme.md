9154055880c3023d928d77be44163cb669fdb3f8
ltcmichelli <42404448+ltcmichelli@users.noreply.github.com>
2020-04-14 14:57:52 +0800

    [Michelle] rename variable

---
+++ readme.md
@@ -0,0 +1,3 @@
+1. Reformat the code
+2. Remove unused import
+3. Rename the variable to meaningful naming
--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -5,21 +5,21 @@ import java.util.Map;
 import java.util.StringJoiner;
 
 public class WordFrequencyGame {
-    public String getResult(String inputStr) {
+    public String getResult(String inputSentence) {
 
 
-        if (inputStr.split("\\s+").length == 1) {
-            return inputStr + " 1";
+        if (inputSentence.split("\\s+").length == 1) {
+            return inputSentence + " 1";
         } else {
 
             try {
 
                 //split the input string with 1 to n pieces of spaces
-                String[] arr = inputStr.split("\\s+");
+                String[] wordArray = inputSentence.split("\\s+");
 
                 List<WordInfo> wordInfoList = new ArrayList<>();
-                for (String s : arr) {
-                    WordInfo wordInfo = new WordInfo(s, 1);
+                for (String word : wordArray) {
+                    WordInfo wordInfo = new WordInfo(word, 1);
                     wordInfoList.add(wordInfo);
                 }
 
@@ -33,15 +33,15 @@ public class WordFrequencyGame {
                 }
                 wordInfoList = list;
 
-                wordInfoList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+                wordInfoList.sort((firstWordInfo, secondWordInfo) -> secondWordInfo.getWordCount() - firstWordInfo.getWordCount());
 
                 StringJoiner joiner = new StringJoiner("\n");
-                for (WordInfo w : wordInfoList) {
-                    String s = w.getWord() + " " + w.getWordCount();
-                    joiner.add(s);
+                for (WordInfo wordInfo : wordInfoList) {
+                    String wordWithCount = wordInfo.getWord() + " " + wordInfo.getWordCount();
+                    joiner.add(wordWithCount);
                 }
                 return joiner.toString();
-            } catch (Exception e) {
+            } catch (Exception exception) {
                 return "Calculate Error";
             }
         }
@@ -52,9 +52,9 @@ public class WordFrequencyGame {
         for (WordInfo wordInfo : wordInfoList) {
 //       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
             if (!map.containsKey(wordInfo.getWord())) {
-                ArrayList arr = new ArrayList<>();
-                arr.add(wordInfo);
-                map.put(wordInfo.getWord(), arr);
+                ArrayList arrayList = new ArrayList<>();
+                arrayList.add(wordInfo);
+                map.put(wordInfo.getWord(), arrayList);
             } else {
                 map.get(wordInfo.getWord()).add(wordInfo);
             }
